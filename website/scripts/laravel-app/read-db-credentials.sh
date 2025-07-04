e5e0238d7f08382f2e8979d8c4d04498f7f9d6ce
Zeepaardje <rvaken98@gmail.com>
2025-07-04 12:32:32 +0200

    split read-db-credentials so the testrunner does not have access to main db credentials

--- website/scripts/laravel-app/read-db-credentials.sh
+++ website/scripts/laravel-app/read-db-credentials.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 set -e
 
-# Source root password from Docker secret
-# if [ -f /run/secrets/mysql_db_rootpass ]; then
-#   DB_ROOT_PASSWORD=$(cat /run/secrets/mysql_db_rootpass | tr -d '\r\n')
-#   export DB_ROOT_PASSWORD
-# fi
-
-# Source main app db credentials from split secrets
+# Source main app db credentials from Docker secrets
 if [ -f /run/secrets/mysql_app_db_username ]; then
   DB_USERNAME=$(cat /run/secrets/mysql_app_db_username | tr -d '\r\n')
   export DB_USERNAME
@@ -19,18 +13,4 @@ fi
 if [ -f /run/secrets/mysql_app_db_database ]; then
   DB_DATABASE=$(cat /run/secrets/mysql_app_db_database | tr -d '\r\n')
   export DB_DATABASE
-fi
-
-# Source testing db credentials from split secrets
-if [ -f /run/secrets/mysql_testing_db_username ]; then
-  DB_USER_TESTING=$(cat /run/secrets/mysql_testing_db_username | tr -d '\r\n')
-  export DB_USER_TESTING
-fi
-if [ -f /run/secrets/mysql_testing_db_password ]; then
-  DB_PASSWORD_TESTING=$(cat /run/secrets/mysql_testing_db_password | tr -d '\r\n')
-  export DB_PASSWORD_TESTING
-fi
-if [ -f /run/secrets/mysql_testing_db_database ]; then
-  DB_DATABASE_TESTING=$(cat /run/secrets/mysql_testing_db_database | tr -d '\r\n')
-  export DB_DATABASE_TESTING
 fi
