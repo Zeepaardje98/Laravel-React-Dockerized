67f271a362a2f0e3bdf96d245b56fc20fd4d7269
Zeepaardje <rvaken98@gmail.com>
2025-07-04 13:18:41 +0200

    secrets are now properly used by db cloning scripts and laravel container. Placed new method of running artisan commands in scripts/laravel-app/commands.txt

--- website/scripts/laravel-app/clone_db_for_testing.sh
+++ website/scripts/laravel-app/clone_db_for_testing.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# Hardcoded database connection variables (TEMPORARY. REMOVE LATER)
 DB_HOST=db
 DB_PORT=3306
-DB_DATABASE=laravel_app_db
-DB_USERNAME=admin
-DB_PASSWORD=password
+
+# Source testing user DB credentials from Docker secrets
+. /read-db-credentials-testing.sh
+
+# Get the main DB name (testing user has read access to this DB)
+if [ -f /run/secrets/mysql_app_db_database ]; then
+  DB_DATABASE=$(cat /run/secrets/mysql_app_db_database | tr -d '\r\n')
+  export DB_DATABASE
+fi
 
 # Drop and recreate the test database
-# mysql -h "db" -P "3306" -u "admin" -p"password" -e "DROP DATABASE IF EXISTS \`laravel_app_db_testing\`;"
-mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "DROP DATABASE IF EXISTS \`${DB_DATABASE}_testing\`;"
-mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "CREATE DATABASE \`${DB_DATABASE}_testing\`;"
+mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" -e "DROP DATABASE IF EXISTS \`$DB_DATABASE_TESTING\`;"
+mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" -e "CREATE DATABASE \`$DB_DATABASE_TESTING\`;"
 
 # Dump the main database schema (and optionally data) and import into test database
-mysqldump -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p"$DB_PASSWORD" "$DB_DATABASE" | mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p"$DB_PASSWORD" "${DB_DATABASE}_testing"
+mysqldump -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" "$DB_DATABASE" | mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" "$DB_DATABASE_TESTING"
